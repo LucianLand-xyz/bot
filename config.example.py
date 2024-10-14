d00436b3f2f15a1cfef56e75f3118c9b12f2d174
Lucian :3 <sfdfdfadfda@gmail.com>
2024-10-14 17:00:48 -0400

    added example

---
+++ config.example.py
@@ -0,0 +1,13 @@
+class Config:
+    DISCORD_CLIENT_ID : str = 1291929962445541427
+    DiscordBotToken : str = "bottoken"
+    DISCORD_CLIENT_SECRET : str = "clientsecret"
+
+    BackendServerUrl : str = "siteurl"
+    BackendAuthenticationToken : str = "backendpassword"
+
+    AuthorisedGuilds : list = [1289705746044420146]
+
+    VerifiedRoleId : int = 1289707999039979530
+    OBCRoleId : int = 1291972280867491901
+    ServerBoosterRole : int = 1291972433506734170
